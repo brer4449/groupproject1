ea7b62b3fcc770ac89fd56f7e07dd1b91ff3808f
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-15 13:19:10 -0500

    made the ajax call for mapquest!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,18 @@
 // Carousel
-$(document).ready(function(){
+$(document).ready(function () {
   $('.carousel').carousel();
+
+  //This lets us put in a specific location (by zip in this case)
+  let mapAPIKey = "19ObWX0Nw2vIDzYqg9vODBXcBzvsPj1l";
+  //original format:
+  //https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false
+  let mapQueryUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${mapAPIKey}&location=2080202&thumbMaps=false`
+
+  $.ajax({
+    url: mapQueryUrl,
+    method: "GET",
+  }).then(function (response) {
+    //check out the console.log! It works ;)
+    console.log(response)
+  })
 });
