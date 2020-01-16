43d442c97c131d08f7f626b6b2a49a22846fc1ac
Katie Durga <katie.durga@gmail.com>
2020-01-16 11:01:26 -0500

    sign data saved to local storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,33 +60,38 @@ var sign = 'cancer';
 
    $("#birthday-input").change(function(){
       var selectedSign = $(this).children("option:selected").val();
-      console.log(selectedSign);
+			console.log(selectedSign);
+			let signToSave = selectedSign;
+			let savedSign = JSON.parse(localStorage.getItem("savedSign")) || [];
+			savedSign.push(signToSave);
+			localStorage.setItem("savedSign", JSON.stringify(savedSign));
    });
 
+	
 
 // Show table on Click
 // $(showLovers).on("click", function () {
 //   $("#weHere").removeClass("hide");
 // })
 
-//MAPQUEST
-let mapAPIKey = "19ObWX0Nw2vIDzYqg9vODBXcBzvsPj1l";
-//original format:
-//https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false
-let mapQueryUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${mapAPIKey}&location=2080202&thumbMaps=false`
-let targetzip = 80303;
-let userzip = 80021;
-//original directions url:
-//https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false
-//This lets us put in directions (by zip code)
-mapQueryUrl = `https://www.mapquestapi.com/directions/v2/route?key=${mapAPIKey}&from=${userzip}&to=${targetzip}&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false`
+// //MAPQUEST
+// let mapAPIKey = "19ObWX0Nw2vIDzYqg9vODBXcBzvsPj1l";
+// //original format:
+// //https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false
+// let mapQueryUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${mapAPIKey}&location=2080202&thumbMaps=false`
+// let targetzip = 80303;
+// let userzip = 80021;
+// //original directions url:
+// //https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false
+// //This lets us put in directions (by zip code)
+// mapQueryUrl = `https://www.mapquestapi.com/directions/v2/route?key=${mapAPIKey}&from=${userzip}&to=${targetzip}&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false`
 
 
-$.ajax({
-  url: mapQueryUrl,
-  method: "GET",
-}).then(function (response) {
-  //check out the console.log! It works ;)
-  console.log(response.route.distance)
-})
+// $.ajax({
+//   url: mapQueryUrl,
+//   method: "GET",
+// }).then(function (response) {
+//   //check out the console.log! It works ;)
+//   console.log(response.route.distance)
+// })
 });
